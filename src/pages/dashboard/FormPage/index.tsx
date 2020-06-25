cb7f1e5a6d9780fba523701dedaf726edebbb3fb
Diego <diegoayanezvega@hotmail.com>
2020-06-25 15:00:32 -0500

    Agregando el Botón Ok

--- src/pages/dashboard/FormPage/index.tsx
+++ src/pages/dashboard/FormPage/index.tsx
@@ -1,6 +1,6 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import { Spin, Button } from 'antd';
 import styles from './index.less';
 
 export default () => {
@@ -12,6 +12,7 @@ export default () => {
   }, []);
   return (
     <PageHeaderWrapper content="" className={styles.main}>
+        <Button>Ok</Button>
       <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Spin spinning={loading} size="large" />
       </div>
