1bf2b14139f96f94ca68b86693e7450214bfb715
Diego <diegoayanezvega@hotmail.com>
2020-06-26 12:24:15 -0500

    Configuración de linternas

--- src/pages/diego/blankpage/index.jsx
+++ src/pages/diego/blankpage/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const blankPage = () => {
-
   return (
     <>
       <p>I am blankPage</p>
     </>
-  )
-}
+  );
+};
 
 export default blankPage;
--- src/pages/diego/login/index.tsx
+++ src/pages/diego/login/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const login = () => {
-
   return (
     <>
       <p>I am login page</p>
     </>
-  )
-}
+  );
+};
 
 export default login;
